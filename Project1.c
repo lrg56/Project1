b4c410925378db40cd3e08e61d32f586db108275
lrg56 <lrg56@nau.edu>
2016-09-16 18:29:14 -0700

    Main Code
    Added comments and more syntax errors.

--- Project1.c
+++ Project1.c
@@ -1,11 +1,18 @@
+/**
+ *Author: Logan Green
+ *Date:09/15/16
+ *Project 1: PPM image converter
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+//Stores the 3 bites that make up a pixel; value range is 0 to 255
 typedef struct pixel{
   unsigned char r,g,b;
 }pixel;
 
+//Stores image information, read from file
 typedef struct Image{
   char *Magic;
   int w, h;
@@ -13,6 +20,7 @@ typedef struct Image{
   Pixel *data;
 }Image;
 
+//Ensures that the values stored are within the specified range
 int check_bits(int r, int g, int b, int max, int min){
   if(((r > max) || (r < min)) || ((g > max) || (g < min)) || ((b > max) || (b < min))){
     return(1);
@@ -21,7 +29,7 @@ int check_bits(int r, int g, int b, int max, int min){
   }
 
 }
-
+//Reads the information from the file and stores it
 void read(char *fh, Image *image){
   char buffer[64];
   FILE *fp;
@@ -29,23 +37,24 @@ void read(char *fh, Image *image){
   int r, g, b;
   int c;
   
-
+  //opens the file for reading
   fpointer = fopen(filename, "r");
-
+  //If the pointer points to something, then the file opened.
   if(fpointer == NULL){
     fprintf(stderr, "The file was unable to be opened.\n");
     fclose(fpointer);
     exit(1);
   }
-
+  rewind(fp);
+  //get the first two characters, should be the format.
   buffer[0] = fgetc(fpointer);
   buffer[1] = fgetc(fpointer);
-
+  //if P is not the first character then the file is not formatted properly
   if(buffer[0] != 'P'){
     fprintf(stderr, "This file does not have a PPM format\n");
     exit(1);
   }
-
+  //Determine if the file is in P3 or P6 format
   if(buffer[1] == '3'){
     image->Magic = "P3";
   }else if(buffer[1] == '6'){
@@ -54,7 +63,7 @@ void read(char *fh, Image *image){
     fprintf(stderr, "This file needs to be in P3 or P6 format\n");
     exit(1);
   }
-
+  //runs through comments and ignores them
   c = getc(fp);
   while(c == '#'){
     while(getc(fp) != '\n'){
@@ -63,24 +72,24 @@ void read(char *fh, Image *image){
   }
 
   ungetc(c, fp);
-
+  //reads the height and width
   if(fscanf(fp, "%d %d", &image->w, &image->h) != 2){
     fprintf(stderr, "Invalid width or height when reading\n");
     exit(1);
   }
-
+  //reads the max color value
   if(fscanf(fp, "%d", &image->MaxColor) != 1){
     fprintf(stderr, "Invalid max color value when reading\n");
     exit(1);
   }
-
+  //checks to see that the max color value is within range
   if((image->MaxColor < 0) || (image->MaxColor > 255)){
     fprintf(stderr, "Invalid max color value range(>0 or <255)\n");
     exit(1);
   }
-
+  //memory allocation
   image->data = malloc(sizeof(Pixel) * image->w * image->h);
-
+  //Determines the file type then reads it appropriately
   if(image->Magic[1] == '3'){
     for(row = 0; row < image->h; row++){
       for(col = 0; col < image->w; col++){
@@ -106,8 +115,9 @@ void read(char *fh, Image *image){
     fprintf(stderr, "There was an error reading the PPM file number(it should be P3 or P6)\n");
     exit(1);
   }
+  fclose(fp);
 }
-
+//Writes the file into the P3 binary format.
 void write_P3(char *fh, Image *image){
   char buffer[64];
   int row, col;
@@ -138,6 +148,8 @@ void write_P3(char *fh, Image *image){
     fclose(fpointer);
   }
 }
+
+//Writes the file into the ASCII P6 format
 void write_P6(char *fh, Image *image){
   FILE *fp;
   fp = fopen(fh, "wb");
@@ -154,19 +166,19 @@ void write_P6(char *fh, Image *image){
   fclose(fp);
 }
 
-
+//main function, takes in the program name, the PPM conversion number(3 or 6), the filename of the image to be converted and the filename to output the data into.
 int main(int argc, char *argv[]){
   Image *PPM_image;
   PPM_image = (Image *)malloc(sizeof(Image));
-
+  //checkes to ensure the correct number of inputs
   if(argc != 4){
     fprintf(stderr, "Incorrect number of arguments please check they are correct");
     exit(1);
-
+    //checks to ensure that the correct number(3 or 6) is used
   if(atoi(argv[1]) == 3){
     read(argv[2], PPM_image);
     write_P3(argv[3], PPM_image);
-  }else if(atoi(argv[1]) == 3){
+  }else if(atoi(argv[1]) == 6){
     read(argv[2], PPM_image);
     write_P6(argv[3], PPM_image);
   }else{
